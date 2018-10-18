492edc558b98c8a7930875dde2af9433f0ce9a0d
Miguel Morales <therevoltingx@gmail.com>
2018-10-17 17:06:37 -0700

    chore(bank): removes unused challenge mechanism

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -10,17 +10,6 @@ import 'token-sale-contracts/contracts/HumanStandardToken.sol';
 contract Bank is Ownable, ReentrancyGuard {
   address public tokenAddress;
   address public chainAddress;
-  uint256 public balance;
-
-  uint256 challengeWaitingRounds = 2;
-
-  struct Account {
-    uint256 withdrawing;
-    uint256 withdrawingAt;
-    uint256 withdrawingShard;
-  }
-
-  mapping(address => Account) public accounts;
 
   constructor(
     address _tokenAddress,
@@ -34,7 +23,7 @@ contract Bank is Ownable, ReentrancyGuard {
    * @dev Callback for standard ERC-20 token accepting contracts and is called when accepting deposits.
    */
 
-  function receiveApproval (
+  function receiveApproval(
     address _from,
     uint256 _value,
     address _token,
@@ -48,99 +37,26 @@ contract Bank is Ownable, ReentrancyGuard {
     require(allowance > 0);
     require(token.transferFrom(_from, this, allowance));
 
-    balance += allowance;
-
     return true;
   }
 
-  /**
-   * @dev Initiates an exit by providing proof-of-balance against last valid sidechain block.
-   */
-
-  function startWithdrawal(
+  function withdraw(
     uint256 _shard,
     uint256 _amount,
+    uint256 _balance,
     bytes32[] _proof
   ) public nonReentrant returns (bool success) {
     bytes32 root = lastValidBlockRootForShard(_shard);
 
-    bytes32 leafValue = keccak256(abi.encodePacked(tokenAddress, msg.sender, _amount));
+    bytes32 leafValue = keccak256(abi.encodePacked(tokenAddress, msg.sender, _balance));
 
     require(MerkleProof.verifyProof(_proof, root, leafValue));
 
-    Account storage account = accounts[msg.sender];
-
-    require(account.withdrawing == 0);
-
-    account.withdrawing = _amount;
-    account.withdrawingAt = block.number;
-    account.withdrawingShard = _shard;
-
-    return true;
-  }
-
-  /**
-   * @dev During an exit's challenge period, anyone is able to submit proof of lower balance against the last valid block.
-   */
-
-  function challengeWithdrawal(
-    address _withdrawer,
-    uint256 _balance,
-    bytes32[] _proof
-  ) public nonReentrant returns (bool success) {
-    Chain chain = Chain(chainAddress);
-
-    Account storage account = accounts[_withdrawer];
-    require(account.withdrawing > 0);
-
-    require(account.withdrawing > _balance);
-
-    uint256 challengePeriod = chain.blocksPerPhase() * (challengeWaitingRounds * 2);
-
-    require((block.number - account.withdrawingAt) < challengePeriod);
-
-    bytes32 root = lastValidBlockRootForShard(account.withdrawingShard);
-
-    bytes32 leafValue = keccak256(abi.encodePacked(tokenAddress, _withdrawer, _balance));
-
-    require(MerkleProof.verifyProof(_proof, root, leafValue));
-
-    uint256 bounty = uint256((account.withdrawing * 2) / 20);
+    require(_amount <= _balance);
 
     Token token = Token(tokenAddress);
 
-    require(token.transferFrom(this, msg.sender, bounty));
-
-    balance -= bounty;
-
-    account.withdrawing = 0;
-    account.withdrawingAt = 0;
-
-    return true;
-  }
-
-  /**
-   * @dev After an exit's challenge period, withdraw the funds if unchallenged.
-   */
-
-  function completeWithdrawal() public nonReentrant returns (bool success) {
-    Chain chain = Chain(chainAddress);
-
-    Account storage account = accounts[msg.sender];
-    require(account.withdrawing > 0);
-
-    uint256 challengePeriod = chain.blocksPerPhase() * (challengeWaitingRounds * 2);
-
-    require((block.number - account.withdrawingAt) >= challengePeriod);
-
-    Token token = Token(tokenAddress);
-
-    require(token.transferFrom(this, msg.sender, account.withdrawing));
-
-    balance -= account.withdrawing;
-
-    account.withdrawing = 0;
-    account.withdrawingAt = 0;
+    require(token.transferFrom(this, msg.sender, _amount));
 
     return true;
   }
